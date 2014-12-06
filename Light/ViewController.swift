e7b40d1153c62d243c5c27d5add7489dc947bca9
Elena Da Re <elenadare@Elenas-MacBook-Pro.local>
2014-12-06 11:16:49 -0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Elena Da Re on 12/6/14.
+//  Copyright (c) 2014 Elena Da Re. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
